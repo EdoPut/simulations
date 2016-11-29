de2413628621775cfed4f3f8cf28882de682d4a9
Edoardo Putti <edoardo.putti@gmail.com>
2016-11-29 10:00:06 +0100

    firts commit

---
+++ scripts/stirling.m
@@ -0,0 +1,19 @@
+% compute the n-th order stirling numbers
+% of second species for n>= 2
+% 
+% octave has a hard limit on recursion; 256 calls are the most
+% you can make without raising an error
+% set max_recursion_depth to your value
+function x = stirling (n)
+  if (n == 2)
+    x =[1,1];
+    return;
+  endif
+  % compute the previous order solution
+  y = stirling(n-1);
+  a = 2 : n-1;
+  % S_{i-1} + i S_{i}
+  temp =  y(1:end-1) + (y(2:end) .* a);
+  x = [1, temp, 1];
+endfunction
+
