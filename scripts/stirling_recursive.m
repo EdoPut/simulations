052d05c6111d95c9a5c1ad897b784d88aaee8c8a
Edoardo Putti <edoardo.putti@gmail.com>
2016-11-29 11:02:15 +0100

    change function name to be a function file

--- scripts/stirling_recursive.m
+++ scripts/stirling_recursive.m
@@ -4,7 +4,7 @@
 % octave has a hard limit on recursion; 256 calls are the most
 % you can make without raising an error
 % set max_recursion_depth to your value
-function x = stirling (n)
+function x = stirling_recursive (n)
   if (n == 2)
     x =[1,1];
     return;
