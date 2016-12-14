b84d7a4018bcdb473879da855d6eddf9a3922a5c
Edoardo Putti <edoardo.putti@gmail.com>
2016-12-14 13:02:41 +0100

    handle complex values in count

--- scripts/count.m
+++ scripts/count.m
@@ -5,6 +5,26 @@
 % count_x :: [numeric]
 % unique_x :: [numeric]
 function [count_x, unique_x] = count (x)
-  unique_x = unique(x);
-  count_x = histc(x, unique_x);
+  [unique_x, to_unique, from_unique] = unique(x);
+
+  % order :: i32
+  order = length(unique_x);
+
+  % count_x :: [i32; order]
+  count_x = zeros(order,1);
+
+  % iterate over the k unique values position
+  parfor i = 1:order
+
+    % for every position in from_unique
+    % check if matches the i-th position
+    % in unique_x
+    for j = from_unique
+      % check, add count
+      if i == j
+        count_x(i)++;
+      endif
+    endfor
+
+    endparfor
 endfunction
